a25eeec15019d44217140802f58f07810199b478
Trevor Green <Trevor@mailagc.com>
2025-04-11 00:11:12 -0700

    Add certifi dependency to formula

--- Formula/disscli.rb
+++ Formula/disscli.rb
@@ -9,6 +9,11 @@ class Disscli < Formula
 
   depends_on "python@3.13"
 
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/71/da/e94e26401b62acd6d91df2b52954aceb7f561743aa5ccc32152886c76c96/certifi-2024.2.2.tar.gz"
+    sha256 "0569859f95fc761b18b45ef421b1290a0f65f147e92a1e5eb3e635f9a5e4e66f"
+  end
+
   resource "charset-normalizer" do
     url "https://files.pythonhosted.org/packages/63/09/c1bc53dab74b1816a00d8d030de5bf98f724c52c1635e07681d312f20be8/charset-normalizer-3.3.2.tar.gz"
     sha256 "f30c3cb33b24454a82faecaf01b19c18562b1e89558fb6c56de4d9118a032fd5"
