74e83f048552f10d60b7728de1c4e7ff1b8770bf
Trevor Green <Trevor@mailagc.com>
2025-04-10 23:49:28 -0700

    feat: update disscli to version 0.1.2

--- Formula/disscli.rb
+++ Formula/disscli.rb
@@ -3,15 +3,15 @@ class Disscli < Formula
 
   desc "A Discord CLI tool for sending messages via webhooks"
   homepage "https://github.com/Trevogre/discordcli"
-  url "https://github.com/Trevogre/discordcli/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "eea7dde37cf7d644c1df922c4f4fd349dfc3f66c8cccf8fa4dd1ee215a5bf8c2"
+  url "https://github.com/Trevogre/discordcli/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "0a1ece3a021f4e27248572b033880f0d44b0cf0f4a4cfe3a61038e75b5ceb84c"
   license "MIT"
 
   depends_on "python@3.13"
 
   resource "requests" do
     url "https://files.pythonhosted.org/packages/9d/be/10918a2eac4ae9f02f6cfe6414b7a155ccd8f7f9d4380d62fd5b955065c3/requests-2.31.0.tar.gz"
-    sha256 "eea7dde37cf7d644c1df922c4f4fd349dfc3f66c8cccf8fa4dd1ee215a5bf8c2"
+    sha256 "0a1ece3a021f4e27248572b033880f0d44b0cf0f4a4cfe3a61038e75b5ceb84c"
   end
 
   def install
