34d06d7ba8fb932b643525066ef144a715b45102
Trevor Green <Trevor@mailagc.com>
2025-04-11 00:58:54 -0700

    feat: update disscli to version 0.1.8

--- Formula/disscli.rb
+++ Formula/disscli.rb
@@ -3,30 +3,30 @@ class Disscli < Formula
 
   desc "A Discord CLI tool for sending messages via webhooks"
   homepage "https://github.com/Trevogre/discordcli"
-  url "https://github.com/Trevogre/discordcli/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "414bc6535b787febd7567804cc015fee39daab8ad86268f1310a9250697de466"
+  url "https://github.com/Trevogre/discordcli/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "bf5a6dd2dd124017f9ef460b9fe6eac1483b157cb8ded2e65b979fccf81679aa"
   license "MIT"
 
   depends_on "python@3.13"
 
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/1c/ab/c9f1e32b7b1bf505bf26f0ef697775960db7932abeb7b516de930ba2705f/certifi-2025.1.31.tar.gz"
-    sha256 "414bc6535b787febd7567804cc015fee39daab8ad86268f1310a9250697de466"
+    sha256 "3d5da6925056f6f18f119200434a4780a94263f10d1c21d032a6f6b2baa20651"
   end
 
   resource "charset-normalizer" do
     url "https://files.pythonhosted.org/packages/16/b0/572805e227f01586461c80e0fd25d65a2115599cc9dad142fee4b747c357/charset_normalizer-3.4.1.tar.gz"
-    sha256 "414bc6535b787febd7567804cc015fee39daab8ad86268f1310a9250697de466"
+    sha256 "44251f18cd68a75b56585dd00dae26183e102cd5e0f9f1466e6df5da2ed64ea3"
   end
 
   resource "idna" do
     url "https://files.pythonhosted.org/packages/f1/70/7703c29685631f5a7590aa73f1f1d3fa9a380e654b86af429e0934a32f7d/idna-3.10.tar.gz"
-    sha256 "414bc6535b787febd7567804cc015fee39daab8ad86268f1310a9250697de466"
+    sha256 "12f65c9b470abda6dc35cf8e63cc574b1c52b11df2c86030af0ac09b01b13ea9"
   end
 
   resource "requests" do
     url "https://files.pythonhosted.org/packages/63/70/2bf7780ad2d390a8d301ad0b550f1581eadbd9a20f896afe06353c2a2913/requests-2.32.3.tar.gz"
-    sha256 "414bc6535b787febd7567804cc015fee39daab8ad86268f1310a9250697de466"
+    sha256 "55365417734eb18255590a9ff9eb97e9e1da868d4ccd6402399eaf68af20a760"
   end
 
   resource "urllib3" do
